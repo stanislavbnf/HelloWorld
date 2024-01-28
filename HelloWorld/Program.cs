d4c537eadc470fcc510c2a9b52b34d13635f3a49
stanislavbnf <156532495+stanislavbnf@users.noreply.github.com>
2024-01-27 20:53:32 -0600

    dz3

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -5,3 +5,17 @@ string yourName;
 yourName = "Jane";
 
 Console.WriteLine(yourName);
+
+bool privet = true;
+bool poka = false;
+int int1 = 120;
+int int2 = 220;
+long long1 = 1242342141241;
+long long2 = 1241224132322;
+char char1 =  'a';
+char char2 = 'b';
+
+string str1 = "Privet";
+string str2 = "poka";
+string str3 = str1 + str2;
+string str4 = str1 + str3;
